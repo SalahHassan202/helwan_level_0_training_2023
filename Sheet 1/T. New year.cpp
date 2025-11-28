2995625bf3a03247ac83a8ca98a222b701c9280a
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-11-28 21:58:32 +0200

    Added T. New year Problem

---
+++ Sheet 1/T. New year.cpp
@@ -0,0 +1,28 @@
+// T. New year
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << n * 365 + n / 4;
+}
+
+int main()
+{
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
