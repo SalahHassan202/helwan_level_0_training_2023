433671f47cdc3a482c50bf7587f45915633edbbe
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-11-30 23:39:45 +0200

    Update B. Ouda and Candies.cpp

--- Contest 2/B. Ouda and Candies.cpp
+++ Contest 2/B. Ouda and Candies.cpp
@@ -1,27 +1,39 @@
 // B. Ouda and Candies
-#include <iostream>
-#include <algorithm>
-#include <cmath>
-#include <string>
-#include <iomanip>
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
 using namespace std;
 
+void solve()
+{
+    
+    ll x , n ; 
+    cin >> x >>  n;
+
+    ll r = x % n;     // reminder
+
+    ll total2  = r == 0 ? 0 : n - r;     // total2  ===> extra_candies
+
+    ll total1 = x + total2 ;             // total1  ===> total_candies 
+  
+    cout << total1 << " " <<  total2 << endl;
+    
+
+}
 
 int main()
 {
-    long long x , n ; 
-    cin >> x >>  n;
-
-    long long r = x % n;     // reminder
-
-    long long total2  = r == 0 ? 0 : n - r;     // total2  ===> extra_candies
-
-    long long total1 = x + total2 ;             // total1  ===> total_candies 
-  
-    cout << total1 << " " <<  total2 << endl;
-    return 0 ;
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
 }
-
-
-
 
