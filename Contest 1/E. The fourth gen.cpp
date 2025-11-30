0a40d6d2847ae91a81a4420d078d987a71168a2c
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-11-30 23:33:18 +0200

    Update E. The fourth gen.cpp

--- Contest 1/E. The fourth gen.cpp
+++ Contest 1/E. The fourth gen.cpp
@@ -1,17 +1,30 @@
 // E. The fourth gen
-#include <iostream>
-#include <algorithm>
-#include<cmath>
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
 using namespace std;
+
+void solve()
+{
+     ll a , b , c ,d  ;
+    cin>> a >> b>> c >> d ;
+
+    cout << a * b * c * d << endl;   
+}
 
 int main()
 {
-    
-    long long a , b , c ,d  ;
-    cin>> a >> b>> c >> d ;
-
-    cout << a * b * c * d << endl;
-
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
     return 0;
 }
 
