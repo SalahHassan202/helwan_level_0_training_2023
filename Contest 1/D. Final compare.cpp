a68b2e81a500a39e33fae37c6c3dc1d5207a7547
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-11-30 23:31:38 +0200

    Update D. Final compare.cpp

--- Contest 1/D. Final compare.cpp
+++ Contest 1/D. Final compare.cpp
@@ -1,13 +1,16 @@
 // D. Final compare
 
-#include <iostream>
-#include <algorithm>
-#include<cmath>
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
 using namespace std;
 
-int main()
+void solve()
 {
-    int x, y;
+       int x, y;
     cin >> x >> y;
 
     if (x == 1)
@@ -33,6 +36,22 @@ int main()
     }
 
 
+
+ 
+
+
+   
+}
+
+int main()
+{
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
     return 0;
 }
 
