6fab78bbf0840bc0359ab856d2706d6363dfd932
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-11-30 23:53:38 +0200

    Added F. Hexadecimal's theorem Problem

---
+++ Contest 2/F. Hexadecimal's theorem.cpp
@@ -0,0 +1,28 @@
+// F. Hexadecimal's theorem
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << "0 0 " << n;
+}
+
+int main()
+{
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
