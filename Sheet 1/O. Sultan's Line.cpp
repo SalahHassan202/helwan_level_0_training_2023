1410b7f73eaea0de12f44b620aa208ee07bba5ef
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-12-06 00:11:11 +0200

    Update O. Sultan's Line.cpp

--- Sheet 1/O. Sultan's Line.cpp
+++ Sheet 1/O. Sultan's Line.cpp
@@ -27,12 +27,18 @@
 //************************************************************************** */
 
 // O. Sultan's Line
-# include<iostream>
-#include <cmath>              
-using namespace std ;
-int main()
-{
 
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+   
     double x1, y1, x2, y2, x3 , y3 ;
     cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 ;
     // عشان تكون ال3 نقاط علي استقامه واحده لازم المحدد = صفر
@@ -46,8 +52,16 @@ int main()
     {
         cout << "No" << endl;
     }
+}
 
-
-
-    return 0 ; 
+int main()
+{
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
 }
