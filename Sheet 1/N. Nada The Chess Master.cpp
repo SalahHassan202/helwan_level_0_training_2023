17f34379c3c434502efc497a9a89d04e969b21ae
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-12-06 00:09:58 +0200

    Update N. Nada The Chess Master.cpp

--- Sheet 1/N. Nada The Chess Master.cpp
+++ Sheet 1/N. Nada The Chess Master.cpp
@@ -1,14 +1,22 @@
 // N. Nada The Chess Master
-#include <iostream>
+
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
 using namespace std;
 
-int main() {
+void solve()
+{
+   
     char column;
     char row;
     cin >> column >> row;
     
-    int col = column - 'a' + 1; // 'a' becomes 1, 'b' becomes 2, ..., 'h' becomes 8
-    int r = row - '0';           // '1' becomes 1, '2' becomes 2, ..., '8' becomes 8
+    int col = column - 'a' + 1;   // 'a' becomes 1, 'b' becomes 2, ..., 'h' becomes 8
+    int r = row - '0';            // '1' becomes 1, '2' becomes 2, ..., '8' becomes 8
     
     int topRight = min(8 - col, 8 - r);
     int topLeft = min(col - 1, 8 - r);
@@ -18,6 +26,17 @@ int main() {
     int totalMoves = topRight + topLeft + bottomRight + bottomLeft;
     
     cout << totalMoves << endl;
+}
 
+int main()
+{
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
     return 0;
 }
+
