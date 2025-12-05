c65055574cda17738a59f7fb5226feb39ebb7e2c
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-12-06 00:08:08 +0200

    Update L. Zoldyck's Geometry Test.cpp

--- Sheet 1/L. Zoldyck's Geometry Test.cpp
+++ Sheet 1/L. Zoldyck's Geometry Test.cpp
@@ -1,10 +1,16 @@
 // L. Zoldyck's Geometry Test
-# include<iostream>
-#include <cmath>              // For sqrt
-using namespace std ;
-int main()
-{
 
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
+{
+   
     double r, a;
     cin >> r >> a;
 
@@ -33,8 +39,16 @@ int main()
     {
         cout << "Complex" << endl;
     }
+}
 
-    
-
-    return 0 ; 
+int main()
+{
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
 }
