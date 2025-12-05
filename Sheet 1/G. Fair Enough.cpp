991c8bfd8d6e77cf2e62c0ea86e789b896fa5ca3
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-12-06 00:04:01 +0200

    Update G. Fair Enough.cpp

--- Sheet 1/G. Fair Enough.cpp
+++ Sheet 1/G. Fair Enough.cpp
@@ -1,15 +1,32 @@
 // G. Fair Enough
 
-#include <iostream>
-using namespace std ;
-int main()
+#include <bits/stdc++.h>
+#define ll long long
+#define fast                     \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+using namespace std;
+
+void solve()
 {
+   
     float Length;
     cin >> Length; 
 
     cout << Length / 2 << endl;
-    
-
-    return 0 ;
 
 }
+
+int main()
+{
+    fast;
+    ll t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
+
